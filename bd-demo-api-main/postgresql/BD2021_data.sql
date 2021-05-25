c5a93b70bc7f7c21a1c3bb5fd462c8caf4f24962
bernamg <berna.graca99@gmail.com>
2021-05-25 15:19:58 +0100

    psu tabelas bem criadas

--- bd-demo-api-main/postgresql/BD2021_data.sql
+++ bd-demo-api-main/postgresql/BD2021_data.sql
@@ -11,7 +11,16 @@ CREATE TABLE bid (
 	username		 VARCHAR(512) NOT NULL,
 	bid_price		 FLOAT(8) NOT NULL,
 	auction_artigo_ean BIGINT,
+	users_username	 VARCHAR(512) NOT NULL,
 	PRIMARY KEY(bid_price,auction_artigo_ean)
+);
+
+CREATE TABLE users (
+	username	 VARCHAR(512),
+	email	 VARCHAR(512) UNIQUE NOT NULL,
+	password	 VARCHAR(512) NOT NULL,
+	token_login INTEGER,
+	PRIMARY KEY(username)
 );
 
 CREATE TABLE edition (
@@ -22,22 +31,34 @@ CREATE TABLE edition (
 
 CREATE TABLE message (
 	text		 TEXT,
+	users_username	 VARCHAR(512),
 	auction_artigo_ean BIGINT,
-	PRIMARY KEY(auction_artigo_ean)
+	PRIMARY KEY(users_username,auction_artigo_ean)
 );
 
 CREATE TABLE notification (
 	message_notif	 TEXT,
 	hour		 DATE,
+	users_username	 VARCHAR(512),
 	auction_artigo_ean BIGINT,
-	PRIMARY KEY(auction_artigo_ean)
+	PRIMARY KEY(users_username,auction_artigo_ean)
+);
+
+CREATE TABLE users_auction (
+	users_username	 VARCHAR(512),
+	auction_artigo_ean BIGINT,
+	PRIMARY KEY(users_username,auction_artigo_ean)
 );
 
 ALTER TABLE bid ADD CONSTRAINT bid_fk1 FOREIGN KEY (auction_artigo_ean) REFERENCES auction(artigo_ean);
+ALTER TABLE bid ADD CONSTRAINT bid_fk2 FOREIGN KEY (users_username) REFERENCES users(username);
 ALTER TABLE edition ADD CONSTRAINT edition_fk1 FOREIGN KEY (auction_artigo_ean) REFERENCES auction(artigo_ean);
-ALTER TABLE message ADD CONSTRAINT message_fk1 FOREIGN KEY (auction_artigo_ean) REFERENCES auction(artigo_ean);
-ALTER TABLE notification ADD CONSTRAINT notification_fk1 FOREIGN KEY (auction_artigo_ean) REFERENCES auction(artigo_ean);
-
+ALTER TABLE message ADD CONSTRAINT message_fk1 FOREIGN KEY (users_username) REFERENCES users(username);
+ALTER TABLE message ADD CONSTRAINT message_fk2 FOREIGN KEY (auction_artigo_ean) REFERENCES auction(artigo_ean);
+ALTER TABLE notification ADD CONSTRAINT notification_fk1 FOREIGN KEY (users_username) REFERENCES users(username);
+ALTER TABLE notification ADD CONSTRAINT notification_fk2 FOREIGN KEY (auction_artigo_ean) REFERENCES auction(artigo_ean);
+ALTER TABLE users_auction ADD CONSTRAINT users_auction_fk1 FOREIGN KEY (users_username) REFERENCES users(username);
+ALTER TABLE users_auction ADD CONSTRAINT users_auction_fk2 FOREIGN KEY (auction_artigo_ean) REFERENCES auction(artigo_ean);
 
 
 INSERT INTO users VALUES('dvm18','dvm@student.uc','123');
